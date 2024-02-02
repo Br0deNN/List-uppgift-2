bdf93a9e11e7c35b93af64dfa28ada459434c795
7R1XY <theo.broden@gmail.com>
2024-02-02 12:44:05 +0100

    Add project files.

---
+++ List uppgift 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+
+        list.Add(5);
+        list.Add(6);
+        list.Add(7);
+        Max(list);
+    }
+
+    static void Max(List<int>Lista)
+    {
+        Lista.Sort();
+        Lista.RemoveRange(0, Lista.Count -1);
+        foreach (int i in Lista)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
